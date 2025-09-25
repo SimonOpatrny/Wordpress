168c75111b4f2d0021f200f03aba4dc750bbafd9
SimonOpatrny <simikopatrny@gmail.com>
2025-09-25 11:32:25 +0200

    nevim

--- wp-content/themes/moje-sablona/index.php
+++ wp-content/themes/moje-sablona/index.php
@@ -1,11 +1,27 @@
-<?php
-get_header();
-?>
+<!DOCTYPE html>
+<html <?Php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        <?php
+            get_header();   
+        ?>
  
-<h1>Ahoj, tohle je moje první šablona!</h1>
- 
-<?php
-get_footer();
-?>
+        <main id="primary" class="site-main">
+        <?php
+        the_content();
+        ?>
+
+        <?php
+            get_footer();
+        ?>
+
+    
+    </body>
+</html>
+
 
 
